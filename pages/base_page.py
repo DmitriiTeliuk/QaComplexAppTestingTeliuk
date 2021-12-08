15b1188f95fe5e219f640c1543408b42e5f46552
Dmitrii Teliuk <dmitryt@elafris.com>
2021-12-08 11:25:22 +0200

    Added basepage methods

---
+++ pages/base_page.py
@@ -0,0 +1,12 @@
+import logging
+
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.log = logging.getLogger(__name__)
+
+    def fill_field(self, by, locator, value):
+        user_field = self.driver.find_element(by=by, value=locator)
+        user_field.clear()
+        user_field.send_keys(value)
